153de81edee78b183a3343c9c62d2b7d7cfb8040
thatshowitworks <andrewfinn39@gmail.com>
2020-04-25 17:52:44 +0000

    Replace admin_tools.py

--- admin_tools.py
+++ admin_tools.py
@@ -29,27 +29,34 @@ logger = logging.getLogger(__name__)
 class BanMod(loader.Module):
     """Group administration tasks"""
     strings = {"name": "Administration",
-               "not_supergroup": "<b>This isn't a supergroup!</b>",
-               "not_group": "<b>This isn't a group!</b>",
+               "ban_not_supergroup": "<b>I can't ban someone unless they're in a supergroup!</b>",
+               "unban_not_supergroup": "<b>I can't unban someone unless they're banned from a supergroup!</b>",
+               "kick_not_group": "<b>I can't kick someone unless they're in a group!</b>",
+               "mute_not_supergroup": "<b>I can't mute someone unless they're in a supergroup!</b>",
+               "unmute_not_supergroup": "<b>I can't un-mute someone unless they're in a supergroup!</b>",
                "ban_none": "<b>I can't ban no-one, can I?</b>",
                "unban_none": "<b>I need someone to unbanned here.</b>",
                "kick_none": "<b>I need someone to be kicked out of the chat.</b>",
                "promote_none": "<b>I can't promote no one, can I?</b>",
                "demote_none": "<b>I can't demote no one, can I?</b>",
+               "mute_none": "<b>I can't mute no-one, can I?</b>",
+               "unmute_none": "<b>I can't unmute no-one, can I?</b>",
                "who": "<b>Who the hell is that?</b>",
                "not_admin": "<b>Am I an admin here?</b>",
                "banned": "<b>Banned</b> <code>{}</code> <b>from the chat!</b>",
                "unbanned": "<b>Unbanned</b> <code>{}</code> <b>from the chat!</b>",
                "kicked": "<b>Kicked</b> <code>{}</code> <b>from the chat!</b>",
                "promoted": "<code>{}</code> <b>is now powered with admin rights!</b>",
-               "demoted": "<code>{}</code> <b>is now stripped off of their admin rights!</b>"}
+               "demoted": "<code>{}</code> <b>is now stripped off of their admin rights!</b>",
+               "muted": "<b>Muted</b> <code>{}</code> <b>in the chat!</b>",
+               "unmuted": "<b>Unmuted</b> <code>{}</code> <b>in the chat!</b>"}
 
     @loader.group_admin_ban_users
     @loader.ratelimit
     async def bancmd(self, message):
         """Ban the user from the group"""
         if not isinstance(message.to_id, PeerChannel):
-            return await utils.answer(message, self.strings("not_supergroup", message))
+            return await utils.answer(message, self.strings("ban_not_supergroup", message))
         if message.is_reply:
             user = await utils.get_user(await message.get_reply_message())
         else:
@@ -192,6 +199,56 @@ class BanMod(loader.Module):
             await self.allmodules.log("demote", group=message.chat_id, affected_uids=[user.id])
             await utils.answer(message,
                                self.strings("demoted", message).format(utils.escape_html(ascii(user.first_name))))
+
+    async def mutecmd(self, message):
+        """Mutes the user in the group"""
+        if not isinstance(message.to_id, PeerChannel):
+            return await utils.answer(message, self.strings("mute_not_supergroup", message))
+        if message.is_reply:
+            user = await utils.get_user(await message.get_reply_message())
+        else:
+            args = utils.get_args(message)
+            if len(args) == 0:
+                return await utils.answer(message, self.strings("mute_none", message))
+            user = await self.client.get_entity(args[0])
+        if not user:
+            return await utils.answer(message, self.strings("who", message))
+        logger.debug(user)
+        try:
+            await self.client(EditBannedRequest(message.chat_id, user.id,
+                                                ChatBannedRights(until_date=None, send_messages=True)))
+        except BadRequestError:
+            await utils.answer(message, self.strings("not_admin", message))
+        else:
+            await self.allmodules.log("mute", group=message.chat_id, affected_uids=[user.id])
+            await utils.answer(message, self.strings("muted",
+                                                     message).format(utils.escape_html(ascii(user.first_name))))
+
+    async def unmutecmd(self, message):
+        """Unmutes the user in the group"""
+        if not isinstance(message.to_id, PeerChannel):
+            return await utils.answer(message, self.strings("unmute_not_supergroup", message))
+        if message.is_reply:
+            user = await utils.get_user(await message.get_reply_message())
+        else:
+            args = utils.get_args(message)
+            if len(args) == 0:
+                return await utils.answer(message, self.strings("unmute_none", message))
+            user = await self.client.get_entity(args[0])
+        if not user:
+            return await utils.answer(message, self.strings("who", message))
+        logger.debug(user)
+        try:
+            await self.client(EditBannedRequest(message.chat_id, user.id, ChatBannedRights(until_date=None,
+                                                view_messages=None, send_messages=False, send_media=False,
+                                                send_stickers=False, send_gifs=False, send_games=False,
+                                                send_inline=False, embed_links=False)))
+        except BadRequestError:
+            await utils.answer(message, self.strings("not_admin", message))
+        else:
+            await self.allmodules.log("unmute", group=message.chat_id, affected_uids=[user.id])
+            await utils.answer(message, self.strings("unmuted",
+                                                     message).format(utils.escape_html(ascii(user.first_name))))
 
     async def client_ready(self, client, db):
         self.client = client
